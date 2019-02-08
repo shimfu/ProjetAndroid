fd7bc0ee0017de046ef3e6177c0b2e418aac63b9
shimfu <45978873+shimfu@users.noreply.github.com>
2019-02-08 16:51:01 +0100

    Implementation effects advances
    ajout des effets "avancés" et de leurs sous-classes dans convolution et histogram

--- app/src/main/java/com/example/acoste/projetimage/Advanced.java
+++ app/src/main/java/com/example/acoste/projetimage/Advanced.java
@@ -1,8 +1,14 @@
 package com.example.acoste.projetimage;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.util.Log;
+
 import com.example.acoste.projetimage.Convolution;
 import com.example.acoste.projetimage.Histogram;
+import com.example.q.renderscriptexample.ScriptC_histEq;
 
 
 /**
@@ -11,18 +17,169 @@ import com.example.acoste.projetimage.Histogram;
 
 public abstract class Advanced extends Effects {
 
-    abstract Bitmap blur();
+    int[] blur_data(Bitmap bMap, int k, int[][] mask){// return the colro data of a blured Bitmap
 
-    abstract Bitmap outline();
+        int norme = Convolution.norm_for_blurmask(mask);
 
-    abstract Bitmap linear_contrast_rgb();
+        int[] pixelData = Convolution.mirrorBitmap(bMap, k);
+        int[][] pixelDataRGB = Convolution.convolutionOnMirror( pixelData, k, bMap.getWidth()+2*k, bMap.getHeight()+2*k, mask);
+        int[] pixelDataFinal = new int[bMap.getWidth() * bMap.getHeight()];
 
-    abstract Bitmap linear_contrast_hsv();
+        for (int i = 0 ; i < pixelDataFinal.length ; i++){
+            pixelDataRGB[i][1] = pixelDataRGB[i][1] / norme;
+            pixelDataRGB[i][2] = pixelDataRGB[i][2] / norme;
+            pixelDataRGB[i][3] = pixelDataRGB[i][3] / norme;
+        }
+        for (int i = 0 ; i < pixelDataFinal.length ; i++){
+            pixelDataFinal[i] = Color.argb(pixelDataRGB[i][0], pixelDataRGB[i][1], pixelDataRGB[i][2], pixelDataRGB[i][3]);
+        }
+        return pixelDataFinal;
+    }
 
-    abstract Bitmap equalization_contrast_rgb();
+    void blur(Bitmap bMap , int k, int[][] mask){// apply blur on a Bitmap
 
-    abstract Bitmap equalization_contrast_hsv();
+        int[] pixelData = blur_data(bMap, k, mask);
+        bMap.setPixels(pixelData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+    }
 
-    abstract Bitmap equalization_contrast_yuv_RS();
+    void outline(Bitmap bMap){//apply the outline detection algorithm
+        int width = bMap.getWidth();
+        int height = bMap.getHeight();
+        int[] pixelData = new int[width*height];
+        bMap.getPixels(pixelData, 0, width, 0, 0, width, height);
+        pixelData = Convolution.gradient(Convolution.outline_oriented(pixelData, width, height, Convolution.mask_sobel_horizontal()), Convolution.outline_oriented(pixelData, width, height, Convolution.mask_sobel_vertical()));// we apply gradient for 2 outline oriented
+        bMap.setPixels(pixelData, 0, width, 0, 0, width, height);
+
+    }
+
+    public int[][] linear_contrast_ARGB(Bitmap bMap){//apply linear contrast (ARGB color space) on a Bitmap and return original histogram and associated LUT
+        int[][] histog = new int[2][];//allowing memory for returned value wich are histogram
+        int[] colorData = new int[bMap.getHeight() * bMap.getWidth()];
+        bMap.getPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        histog[0] = Histogram.getGreyHistogram(colorData);
+
+        int[] minmax = Histogram.minmaxHisto(histog[0]);
+
+        histog[1] = Histogram.create_linear_contrast_lut(minmax, histog[0].length);//getting LUT (shades of grey - ARGB color space)
+        for (int i = 0 ; i < colorData.length; i++) {
+            int red = Color.red(colorData[i]);
+            int green = Color.green(colorData[i]);
+            int blue = Color.blue(colorData[i]);
+
+            if (red < minmax[0]){//setting new colors from LUT
+                red = 0;
+            }else if(red > minmax[1]){
+                red = 255;
+            }else{
+                red = histog[1][red];
+            }
+            if (green < minmax[0]){
+                green = 0;
+            }else if(green > minmax[1]){
+                green = 255;
+            }else{
+                green = histog[1][green];
+            }
+            if (blue < minmax[0]){
+                blue = 0;
+            }else if(blue > minmax[1]){
+                blue = 255;
+            }else{
+                blue = histog[1][blue];
+            }
+            colorData[i]= Color.argb(Color.alpha(colorData[i]), red, green, blue);
+        }
+        bMap.setPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        return histog;
+    }
+
+    public int[][] linear_contrast_HSV(Bitmap bMap, int precision){//apply linear contrast (HSV color space) on a Bitmap and return original histogram and associated LUT
+        int [][] histog = new int[2][];//allowing memory for returned value wich are histogram
+        int[] colorData = new int[bMap.getHeight() * bMap.getWidth()];
+        bMap.getPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        histog[0] = Histogram.getValueHistogram(colorData, precision);//getting Value histogram (HSV color space)
+        int[] minmax = Histogram.minmaxHisto(histog[0]);//getting minimum and maximum of the histogram
+        histog[1] = Histogram.create_linear_contrast_lut(minmax, histog[0].length);//creating LUT from original histogram
+
+        float[] HSV = new float[3];
+        for (int i = 0 ; i < colorData.length ; i++) {
+            Color.colorToHSV(colorData[i], HSV);
+            HSV[2] = ((float)(histog[1][(int)(HSV[2] * (histog[1].length - 1))]) / ((float)(histog[1].length - 1)));//lot of casts wich are necessary for float division and modifying the Value
+            colorData[i]= Color.HSVToColor(Color.alpha(colorData[i]), HSV);
+        }
+        bMap.setPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        return histog;
+    }
+
+    public int[][] equalization_contrast_hsv(Bitmap bMap, int precision){//apply plane contrast (HSV color space) on a Bitmap and return original histogram and associated LUT
+        int [][] histog = new int[2][];//allowing memory for returned value wich are histogram
+        int[] colorData = new int[bMap.getHeight() * bMap.getWidth()];
+        bMap.getPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        histog[0] = Histogram.getValueHistogram(colorData, precision);//getting Value histogram (HSV color space)
+        histog[1] = Histogram.create_plane_histog_lut(histog[0]);//Creating LUT wich will be returned
+
+        float[] HSV = new float[3];
+        for (int i = 0 ; i < colorData.length ; i++) {
+            Color.colorToHSV(colorData[i], HSV);
+            HSV[2] = ((float)(histog[1][(int)(HSV[2] * (histog[1].length - 1))]) / ((float)(histog[1].length - 1)));//lot of casts wich are necessary for float division and modifying the Value
+            colorData[i] = Color.HSVToColor(Color.alpha(colorData[1]), HSV);
+        }
+        bMap.setPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        return histog;
+    }
+
+    public int[][] equalization_contrast_argb(Bitmap bMap){//apply plane contrast (ARGB color space) on a Bitmap and return original histogram and associated LUT
+        int [][] histog = new int[2][];//allowing memory for returned value wich are histogram
+        int[] colorData = new int[bMap.getHeight() * bMap.getWidth()];
+        bMap.getPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        histog[0] = Histogram.getGreyHistogram(colorData);//getting Value histogram (ARGB color space)
+        histog[1] = Histogram.create_plane_histog_lut(histog[0]);//Creating LUT wich will be returned
+
+        for (int i = 0 ; i < colorData.length ; i++) {
+            int red = Color.red(colorData[i]);// getting RGB component
+            int green = Color.green(colorData[i]);
+            int blue = Color.blue(colorData[i]);
+
+            red = histog[1][red];
+            green = histog[1][green];
+            blue = histog[1][blue];
+
+            colorData[i]= Color.argb(Color.alpha(colorData[i]), red, green, blue);//on met a jour les data qu'on vas utilisé dans set pixel
+        }
+        bMap.setPixels(colorData, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+        return histog;
+    }
+
+    void equalization_contrast_RS(Bitmap image) {
+        //Get image size
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        //Create new bitmap
+        Bitmap res = image.copy(image.getConfig(), true);
+        //Create renderscript
+        RenderScript rs = RenderScript.create(this);
+        //Create allocation from Bitmap
+        Allocation allocationA = Allocation.createFromBitmap(rs, res);
+        //Create allocation with same type
+        Allocation allocationB = Allocation.createTyped(rs, allocationA.getType());
+        //Create script from rs file.
+        ScriptC_histEq histEqScript = new ScriptC_histEq(rs);
+        //Set size in script
+        histEqScript.set_size(width*height);
+        //Call the first kernel.
+        histEqScript.forEach_root(allocationA, allocationB);
+        //Call the rs method to compute the remap array
+        histEqScript.invoke_createRemapArray();
+        //Call the second kernel
+        histEqScript.forEach_remaptoRGB(allocationB, allocationA);
+        //Copy script result into bitmap
+        allocationA.copyTo(image);
+        //Destroy everything to free memory
+        allocationA.destroy();
+        allocationB.destroy();
+        histEqScript.destroy();
+        rs.destroy();
+    }
 
 }
--- app/src/main/java/com/example/acoste/projetimage/Histogram.java
+++ app/src/main/java/com/example/acoste/projetimage/Histogram.java
@@ -1,6 +1,7 @@
 package com.example.acoste.projetimage;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 
 /**
  * Created by acoste on 01/02/19.
@@ -8,5 +9,84 @@ import android.graphics.Bitmap;
 
 public class Histogram{
 
+    static int[] getGreyHistogram(int[] colorData){//return the grey histogram (ARGB color space)
+        int[] greyHistog = new int[256];
+        for (int i = 0 ; i < colorData.length ; i++){
+            greyHistog[(int)(Color.red(colorData[i]) * 0.30 + Color.green(colorData[i]) * 0.59 + Color.blue(colorData[i]) * 0.11)]++;
+        }
+        return greyHistog;
+    }
+
+    static int[] getCumuledHistogram(int[] histog){//return the cumuled histogram of an histogram
+        int[] histogreturned = new int[histog.length];
+        histogreturned[0] = histog[0]; // we init first term for a recurrence
+        for(int i = 1 ; i < histog.length ; i++){
+            histogreturned[i] = histogreturned[i - 1] + histog[i];
+        }
+        return  histogreturned;
+    }
+
+    static int[] getValueHistogram(int[] colorData, int precision){//return the histogram of luminosity with a fixed precision (HSV color space)
+
+        int[] histo = new int[precision];
+        float[] HSV = new float[3];
+
+        for (int i = 0 ; i < colorData.length ; i++){
+            Color.colorToHSV(colorData[i], HSV);
+            histo[(int)(HSV[2] * (precision - 1))]++;
+        }
+        return histo;
+
+    }
+
+    static int[] minmaxHisto(int[] histo){//return respectivly the minimum and the maximum in a int[2] array
+        int[] minmax = new int[2];
+        minmax[0] = 0;
+        minmax[1] = 0;
+
+        int i=0;
+        while (histo[i] == 0 && i < histo.length){
+            i++;
+        }
+        int j = histo.length-1;
+        while (histo[j] == 0 && j > 0){
+            j--;
+        }
+        minmax[0] = i; // minimum
+        minmax[1] = j; // maximum
+        return minmax;
+    }
+
+    static int[] create_linear_contrast_lut_grey(int[] minmax){//return a LUT for linear contrast (ARGB color space)
+        int[] lut_grey = new int[256];
+        int min = minmax[0];
+        int max = minmax[1];
+        for (int i = 0 ; i < 256 ; i++){
+            lut_grey[i] = (255 * (i - min)) / (max - min);
+        }
+        return lut_grey;
+    }
+
+    static int[] create_linear_contrast_lut(int[] minmax, int range){//return a LUT for linear contrast with range elements (ARGB & HSV color space)
+        int[] lut = new int[range];
+        int min = minmax[0];
+        int max = minmax[1];
+        for (int i = 0 ; i < range ;i++){
+            lut[i] = ((range - 1) * (i - min)) / (max - min);
+        }
+        return lut;
+    }
+
+
+    static int[] create_plane_histog_lut(int[] histo){//return a LUT for plane contrast with range elements (ARGB & HSV color space)
+        int[] lut = new int[histo.length];
+        int[] cumuledhistog;
+        cumuledhistog = getCumuledHistogram(histo);
+
+        for (int i = 0 ; i < histo.length ; i++){
+            lut[i] = (cumuledhistog[i] * (histo.length - 1)) / cumuledhistog[histo.length - 1];//"theoretically" impossible to divide by zero in used functions, we could add a simple test
+        }
+        return  lut;
+    }
 
 }
