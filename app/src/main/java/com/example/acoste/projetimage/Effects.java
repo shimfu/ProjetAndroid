76f3cc4ac5a33b878ebbbbc05af4301fafb93a06
Embraysite <ambroise.coste.1@etu.u-bordeaux.fr>
2019-02-08 16:59:37 +0100

    Update
    Update of effects class and simple class (implementation of old function in simple)

--- app/src/main/java/com/example/acoste/projetimage/Effects.java
+++ app/src/main/java/com/example/acoste/projetimage/Effects.java
@@ -7,11 +7,21 @@ package com.example.acoste.projetimage;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Effects {
 
     Bitmap initialImg;
-    Bitmap stateSave[];
+    List<Bitmap> stateSave;//we will get an abitrary size of 3
+    String statePath[];//the effects the user apply consecutivly(not implemented)
     int currentIndex;
+
+    public Effects(Bitmap bMap){
+        initialImg = bMap;
+        stateSave = new ArrayList<Bitmap>();
+    }
+
 
     public Bitmap getInitialImg() {
         return initialImg;
@@ -21,11 +31,11 @@ public class Effects {
         this.initialImg = initialImg;
     }
 
-    public Bitmap[] getStateSave() {
+    public List<Bitmap> getStateSave() {
         return stateSave;
     }
 
-    public void setStateSave(Bitmap[] stateSave) {
+    public void setStateSave(List<Bitmap> stateSave) {
         this.stateSave = stateSave;
     }
 
@@ -33,24 +43,39 @@ public class Effects {
         return currentIndex;
     }
 
-    public void setcurrentIndex(int currentIndex) {
-        if(currentIndex >= 0 && currentIndex < stateSave.length){
+    public boolean setcurrentIndex(int currentIndex) {
+        if(currentIndex >= 0 && currentIndex < 3){//IL FAUT UNE CONSTANTE SIZEMAX (bonus, calculer sizemax)
             this.currentIndex = currentIndex;
+            return true;
         }
+        return false;
     }
 
     public Bitmap getCurrentImg(){
-        return stateSave[currentIndex];
+        return stateSave.get(currentIndex);
     }
 
     public Bitmap undo(){
         setcurrentIndex(this.currentIndex - 1);
         return getCurrentImg();
+
     }
 
     public Bitmap redo(){
         setcurrentIndex(this.currentIndex + 1);
-        return this.stateSave[currentIndex];
+        return this.stateSave.get(currentIndex);
+    }
+
+    public void save(){
+        if(!setcurrentIndex(this.currentIndex + 1)){
+            stateSave.remove(0);
+            setcurrentIndex(this.currentIndex - 1);
+        }
+        stateSave.add(stateSave.get(currentIndex));
+        setcurrentIndex(this.currentIndex + 1);
+
+
+
     }
 
 
