25ee233a4d5278c725f87da4a3a07a3571042bdd
Libertygos <loison.jules@gmail.com>
2019-02-22 16:25:24 +0100

    Add files via upload

--- app/src/main/java/com/example/acoste/projetimage/EffectsActivity.java
+++ app/src/main/java/com/example/acoste/projetimage/EffectsActivity.java
@@ -5,10 +5,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.effect.Effect;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 
 public class EffectsActivity extends AppCompatActivity {
@@ -56,7 +61,30 @@ public class EffectsActivity extends AppCompatActivity {
         }
         /**  1 - TEMPORARY IMPLEMENTATION - 1 **/
 
+        /**Test du zoom **/
+
+        ImageView mIcon = (ImageView) findViewById(R.id.img_to_modify);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test0);
+        RoundedBitmapDrawable mDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        mDrawable.setCircular(true);
+        mIcon.setImageDrawable(mDrawable);
+
+        mIcon.setOnClickListener(listener_zoom);
+
     }
+
+    private View.OnClickListener listener_zoom= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(EffectsActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
+            PhotoView photoView = mView.findViewById(R.id.imageView);
+            photoView.setImageResource(R.drawable.test0);
+            mBuilder.setView(mView);
+            AlertDialog mDialog = mBuilder.create();
+            mDialog.show();
+        }
+    };
 
     private View.OnClickListener listener_gallery = new View.OnClickListener() {
         @Override
