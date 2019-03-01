dcf4c37dade9a90305e5b399b04e6aed0fd4c815
Florian33850 <45092944+Florian33850@users.noreply.github.com>
2019-03-01 15:34:20 +0100

    add

--- app/src/main/java/com/example/acoste/projetimage/Gallery.java
+++ app/src/main/java/com/example/acoste/projetimage/Gallery.java
@@ -1,16 +1,29 @@
 package com.example.acoste.projetimage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Gallery extends AppCompatActivity{
 
     private Button button_camera = null;
     private Button button_effects = null;
     private Button button_menu = null ;
+    private Button button_gallery = null ;
+    static final int RESULT_LOAD_IMG = 1;
+    private Bitmap selectedImage = null;
+    private ImageView imgView = null;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +37,48 @@ public class Gallery extends AppCompatActivity{
 
         button_menu = (Button) findViewById(R.id.menu_gallery);
         button_menu.setOnClickListener(listener_menu);
+
+        button_gallery = (Button) findViewById(R.id.gallery);
+        button_gallery.setOnClickListener(listener_gallery);
+
+        imgView = findViewById(R.id.imageView);
+
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                selectedImage = BitmapFactory.decodeStream(imageStream);
+                selectedImage = getResizedBitmap(selectedImage, 500);
+                imgView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Une erreur s'est produite",Toast.LENGTH_LONG).show();
+            }
+        }else {
+            Toast.makeText(getApplicationContext(),"Vous n'avez pas choisi d'image", Toast.LENGTH_LONG).show();
+        }
     }
 
     private View.OnClickListener listener_effects = new View.OnClickListener() {
@@ -32,6 +87,12 @@ public class Gallery extends AppCompatActivity{
 
             Intent intent = new Intent(Gallery.this, EffectsActivity.class);
 
+            if(selectedImage != null){
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                selectedImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                intent.putExtra("imageGallery", byteArray);
+            }
             startActivity(intent);
         }
     };
@@ -54,5 +115,16 @@ public class Gallery extends AppCompatActivity{
 
             startActivity(intent);
         }
+    };
+
+    private View.OnClickListener listener_gallery = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+        }
+
     };
 }
